e863c1fab1a9347cb5e140e4ec8a78109a611b19
Saurabh Tenpe <133730394+saurabhtenpe111@users.noreply.github.com>
2025-02-28 15:24:35 +0530

    Add Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("This is java program ");
+        System.out.println("Todays Date: " + new Date());
+    }
+}
