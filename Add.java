759982f8863db7dcf579db7802922bf3c05e57f8
Saurabh Tenpe <133730394+saurabhtenpe111@users.noreply.github.com>
2025-03-03 14:17:53 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,6 +1,6 @@
 class Add {
     public static void main(String[] args) {
         int a=100, b=900;
-        System.out.println("Sum of 10+20="+(a+b));
+        System.out.println("Sum of "+a+"+"+b+"="+(a+b));
     }
 }
